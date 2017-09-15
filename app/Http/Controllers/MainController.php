4cf362e2171f42ccfdb4e4e4077fc649b45673d6
vickyroyibha <vickyroyibha@gmail.com>
2017-09-15 16:23:00 +0700

    Modified Main Dashboard

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 
-class Home1Controller extends Controller
+class MainController extends Controller
 {
     public function show()
     {
